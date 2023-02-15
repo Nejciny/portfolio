4284f64126678f15407cfc6c3a0ede40543f7d54
Nejciny <116084048+Nejciny@users.noreply.github.com>
2023-02-15 13:43:35 +0100

    HERO mouse animation
    HERO mouse animation

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,110 @@ import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 
+import { useEffect } from 'react';
+
+
+
 
 function App() {
+
+  // ---------------------HERO MOUSE ANIMATION---------------------------------------
+
+  useEffect(() => {
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    
+    canvas.width = canvas.getBoundingClientRect().width;
+    canvas.height = canvas.getBoundingClientRect().height;
+    
+    let hue = 1;
+    
+    
+    const particleArray = [];
+    
+    class Particle {
+      constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.size = Math.random() * 6 + 1;
+        this.speedX = Math.random() * 3 - 1.5;
+        this.speedY = Math.random() * 3 - 1.5;
+        this.color = hue;
+      }
+      update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+        if (this.size > 0.2) this.size -= 0.1;
+      }
+      draw() {
+        ctx.fillStyle = 'white';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.closePath();
+        ctx.fillStyle= "hsl("+this.color+",100%,50%)";
+        ctx.fill();
+    
+      }
+    }
+    
+    function init() {
+      particleArray.push(new Particle(canvas.width, canvas.height));
+    }
+    
+    function animate() {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      particleArray.forEach((particle, index) => {
+        if (particle.size <= 0.2) {
+          particleArray.splice(index, 1);
+        } else {
+          particle.update();
+          particle.draw();
+        }
+      });
+      requestAnimationFrame(animate);
+    
+      hue ++;
+    
+    }
+    
+    init();
+    animate();
+
+
+    let offset = 0;
+    
+    canvas.addEventListener('mousemove', function(event) {
+      //  ON SCROLL: the offset is calculated and added to Y coordinate on MOUSE MOVE
+      particleArray.push(new Particle(event.x, event.y + offset));
+    });
+
+    window.addEventListener('scroll',function(){
+      offset = window.scrollY;
+    });
+
+    window.addEventListener('resize',function(){
+      canvas.width = canvas.getBoundingClientRect().width;
+      canvas.height = canvas.getBoundingClientRect().height;
+    });
+
+
+  },[]);
+
+
+//----------------------------------------------------------------------------------------
+
+
   return (
     <div className="App">
       <Header />
+
+      <div className="canvas_container">
       <Home />
       <Skills />
+      <canvas id='canvas'></canvas>
+      </div>
+
+      
       <Projects />
       <Contact />
     </div>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -38,72 +38,80 @@ function Contact() {
 
 
   return (
-    <section id='contact' className="container contact">
+    <div className="canvas_container">
 
-    <div class="animation-container">
-            <div class="moon">
-                <div class="crater crater-1"></div>
-                <div class="crater crater-2"></div>
-                <div class="crater crater-3"></div>
-                <div class="crater crater-4"></div>
-                <div class="crater crater-5"></div>
-                <div class="shadow"></div>
-                <div class="eye eye-l"></div>
-                <div class="eye eye-r"></div>
-                <div class="mouth"></div>
-                <div class="blush blush-1"></div>
-                <div class="blush blush-2"></div>
-            </div>
-            <div class="orbit">
-                <div class="rocket">
-                    {/* <div class="window"></div> */}
-                    <img src="../images/ufo.png" alt="" />
-                </div>
-            </div>
-    </div>
+      <canvas id='canvas2'></canvas>
+
+      <section id='contact' className="container contact">
+
+      <div class="animation-container">
+              <div class="moon">
+                  <div class="crater crater-1"></div>
+                  <div class="crater crater-2"></div>
+                  <div class="crater crater-3"></div>
+                  <div class="crater crater-4"></div>
+                  <div class="crater crater-5"></div>
+                  <div class="shadow"></div>
+                  <div class="eye eye-l"></div>
+                  <div class="eye eye-r"></div>
+                  <div class="mouth"></div>
+                  <div class="blush blush-1"></div>
+                  <div class="blush blush-2"></div>
+              </div>
+              <div class="orbit">
+                  <div class="rocket">
+                      {/* <div class="window"></div> */}
+                      <img src="../images/ufo.png" alt="" />
+                  </div>
+              </div>
+      </div>
 
 
-      <h1>Send me a message</h1>
+        <h1>Send me a message</h1>
 
-      <div className="contact_container">
+        <div className="contact_container">
 
-        <div className="contact_options">
-          <a href="mailto:nejckovac99@gmail.com" className="contact_option">
-            <i class="fa-regular fa-envelope"></i>
-            <h4>Email</h4>
-            <h5>nejckovac99@gmail.com</h5>
-            <button>Contact</button>
-          </a>
+          <div className="contact_options">
+            <a href="mailto:nejckovac99@gmail.com" className="contact_option">
+              <i class="fa-regular fa-envelope"></i>
+              <h4>Email</h4>
+              <h5>nejckovac99@gmail.com</h5>
+              <button>Contact</button>
+            </a>
 
-          <a href="https://m.me/nejc.kovac.14" className="contact_option">
-            <i class="fa-brands fa-facebook-messenger"></i>
-            <h4>Messenger</h4>
-            <h5>Nejc Kovač</h5>
-            <button>Contact</button>
-          </a>
-
-        </div>
-
-        <form ref={form} onSubmit={sendEmail}>
-          <input type="text" name='name' placeholder='Your Full Name' required />
-          <input type="email" name='email' placeholder='Your Email' required />
-          <textarea name="message" placeholder='message' id=""  rows="10" required></textarea>
-          <button type='submit'> Send Message</button>
-        </form>
-
-        <div className="popup_container closed" id='popup_container'>
-          <div className="popup closed" id='popup'>
-            <div className="popup_content">
-              <div className="close_btn" onClick={()=>close_popup()}> <i class="fa-solid fa-xmark"></i></div>
-              <p>Your message has been sent!</p>
-            </div>
+            <a href="https://m.me/nejc.kovac.14" className="contact_option">
+              <i class="fa-brands fa-facebook-messenger"></i>
+              <h4>Messenger</h4>
+              <h5>Nejc Kovač</h5>
+              <button>Contact</button>
+            </a>
 
           </div>
 
-        </div>
-      </div>
+          <form ref={form} onSubmit={sendEmail}>
+            <input type="text" name='name' placeholder='Your Full Name' required />
+            <input type="email" name='email' placeholder='Your Email' required />
+            <textarea name="message" placeholder='message' id=""  rows="10" required></textarea>
+            <button type='submit'> Send Message</button>
+          </form>
 
-    </section>
+          <div className="popup_container closed" id='popup_container'>
+            <div className="popup closed" id='popup'>
+              <div className="popup_content">
+                <div className="close_btn" onClick={()=>close_popup()}> <i class="fa-solid fa-xmark"></i></div>
+                <p>Your message has been sent!</p>
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+
+      </section>
+
+
+
+    </div>
   )
 }
 
